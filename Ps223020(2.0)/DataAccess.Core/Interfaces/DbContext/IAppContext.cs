b429d871acf3a45525de11d2dc4a51c3f24175a0
kayzxc1488 <vovaklinov007@gmail.com>
2022-02-13 17:55:00 +0300

    создали

---
+++ Ps223020(2.0)/DataAccess.Core/Interfaces/DbContext/IAppContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ps223020_2._0_.DataAccess.Core.Interfaces.DbContext
+{
+    public interface IAppContext : IDisposable, IAsyncDisposable
+    {
+
+    }
+}
