2d811b8e92236205078116a3b1d81c071c76bb44
kayzxc1488 <vovaklinov007@gmail.com>
2022-02-20 17:53:24 +0300

    ничего

---
+++ Ps223020(2.0)/Test/start.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ps223020_2._0_.Test
+{
+    public static class start
+    {
+        public static world myworld;
+        public static void StartApp()
+        {
+            myworld = new world(x: 50.0F, y: 100.0F);
+        }
+        
+        public static void StartDay()
+        {
+            bool isDay = false;
+            int duration = myworld.getDurationDayandNight();
+            checkEndDay();
+        }
+    }
+}
---
+++ Ps223020(2.0)/Test/world.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ps223020_2._0_.Test
+{
+    public class world
+    {
+        public world(float x, float y)
+        {
+            sizeX = x;
+            sizeY = y;
+        }
+        public float sizeX;
+        public float sizeY;
+        private int durationDayandNight = 30;
+        public int getDurationDayandNight() 
+        {
+            return durationDayandNight;
+        }
+
+        public void SetDurationDayandNight(int newValue)
+        {
+
+            durationDayandNight = newValue;
+        }
+    }
+}
